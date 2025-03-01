ce9e206e7910429a879e0f98d8f365b3e1bb775d
Kacper Urbanski <kacperurbanski002@gmail.com>
2025-03-01 10:39:47 +0100

    extract history into own viewset

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from tasks.views import TaskViewset
+from tasks.views import TaskViewset, TaskHistoryViewset
 
 router = routers.DefaultRouter()
 
-router.register(r'tasks', TaskViewset, basename="task")
+router.register(r"tasks", TaskViewset, basename="task")
+router.register(r"history", TaskHistoryViewset)
 
 urlpatterns = router.urls
--- tasks/views.py
+++ tasks/views.py
@@ -18,41 +18,19 @@ class TaskViewset(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = TaskFilter
 
-    @action(
-        detail=True,
-        methods=["GET"],
-        url_path="history",
-        url_name="history",
-    )
-    def task_history(self, request: Request, pk=None):
-        task = self.get_object()
-        queryset = task.history
 
-        as_of = request.query_params.get("as_of")
-        if as_of:
-            try:
-                target_date = datetime.fromisoformat(as_of)
-                print("target_date=", target_date)
-                historical = task.history.as_of(target_date)
-                serializer = TaskHistorySerializer(historical)
-                return Response(serializer.data)
-            except ValueError:
-                return Response(
-                    {"error": "Invalid date format (e.g., 2024-01-15T12:00:00Z)"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
+class TaskHistoryViewset(viewsets.ReadOnlyModelViewSet):
+    serializer_class = TaskHistorySerializer
+    queryset = Task.history.all().order_by("-history_date")
 
-        serializer = TaskHistorySerializer(queryset, many=True)
-        return Response(serializer.data)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        "history_date": ["gte", "lte"],
+        "history_type": ["exact"],
+        "id": ["exact"],
+        "user": ["exact"],
+        "status": ["exact"],
+    }
 
-    @action(
-        detail=False,
-        methods=["GET"],
-        url_path="history",
-        url_name="list-history",
-    )
-    def history(self, request: Request):
-        queryset = Task.history.all().order_by("-history_date")
-        serializer = TaskHistorySerializer(queryset, many=True)
-
-        return Response(serializer.data)
+    def get_queryset(self):
+        return self.queryset.select_related("user")
